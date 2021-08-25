e8169ffdceb79fb7a9fc04678034c0944daed9d9
Jihyun Lee <azurer@nate.com>
2021-08-24 22:21:22 -0400

    Added profile pic and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            child: Column(children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/me.jpg'),
+          ),
+          Text('Jihyun Lee',
+              style: TextStyle(
+                fontFamily: 'Lobster',
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 3,
+              )),
+          Text(
+            'Flutter Programmer',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.teal.shade100,
+              letterSpacing: 2.5,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ])),
       ),
-    ),
-  );
+    );
+  }
 }
